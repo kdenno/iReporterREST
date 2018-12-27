99e118094eed112b1ac5074ce50954c0db2403d8
kdenno <kabuyedennis3@gmail.com>
2018-12-28 00:45:51 +0300

    add edit_flag_comment

--- api/redflags/endpoints.py
+++ api/redflags/endpoints.py
@@ -68,3 +68,44 @@ def get_flag(red_flag_id):
             return statusresponse.success_200([value], 'redflag')
     else:
         return statusresponse.error_400('Flag doesnot exist')
+
+
+# edit flag location
+
+
+@redendpoint.route('/red-flags/<red_flag_id>/location', methods=['PATCH'])
+def edit_flag_location(red_flag_id):
+    if red_flag_id == '' or red_flag_id == None:
+        return statusresponse.error_400('No ID passed ')
+
+    # check if flags
+    allredflags = [flag.to_json() for flag in redflags]
+    if len(allredflags) < 1:
+        return jsonify({'status': 204, 'data': [{}]}), 204
+
+    postdata = request.get_json()
+
+    # check for data
+    if not postdata:
+        return statusresponse.error_400('No location data passed ')
+
+    # check its standard
+    required_fields = ['location']
+    if not all(field in postdata for field in required_fields):
+        return statusresponse.error_400('Required parameter(s) missing ')
+
+    for key, value in enumerate(allredflags):
+        if value.get('id') == int(red_flag_id):
+            # edit object data
+            value['location'] = postdata['location']
+
+            # reconvert back to incident object
+            reconverted = Incident(value)
+
+            # replace flag
+            redflags[key] = reconverted
+
+            # return to client
+            return statusresponse.success_201(red_flag_id, 'Updated red-flag record’s location')
+    else:
+        return statusresponse.error_400('Flag doesnot exist')
