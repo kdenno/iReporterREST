a152b14a10d650e0d1c92f4c82b3225fb294da0a
kdenno <kabuyedennis3@gmail.com>
2018-12-28 00:02:10 +0300

    edited comment

--- api/redflags/endpoints.py
+++ api/redflags/endpoints.py
@@ -6,7 +6,7 @@ import api.redflags.statusresponse as statusresponse
 from api.redflags.stoerer import store_func
 
 
-# init blue print with module name
+# initialize blue print with module name
 redendpoint = Blueprint('redflags', __name__)
 
 redflags = store_func()
